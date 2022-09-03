4a9c1354bf446fecde4bca292af2549af227aa74
Karl <ollylut@gmail.com>
2022-09-03 19:26:04 +0200

    fix storybook

---
+++ docs/.storybook/main.js
@@ -0,0 +1,7 @@
+module.exports = {
+	stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
+	addons: ['@storybook/addon-essentials'],
+	framework: '@storybook/react',
+	core: { builder: 'webpack5' },
+	features: { emotionAlias: false }
+}
