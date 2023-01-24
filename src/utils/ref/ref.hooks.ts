a8efc3dd395413476e91091e7f8267e77eb92f82
daleksandrov <ollylut@gmail.com>
2023-01-24 16:18:39 +0100

    feat docs - update fork-ref

--- src/utils/ref/ref.hooks.ts
+++ src/utils/ref/ref.hooks.ts
@@ -1,4 +1,4 @@
-import { MutableRefObject, Ref, useMemo } from 'react'
+import { type Ref, type RefCallback, type MutableRefObject, useMemo } from 'react'
 
 const setRef = <T>(
 	ref: MutableRefObject<T | null> | ((instance: T | null) => void) | null | undefined,
@@ -8,22 +8,23 @@ const setRef = <T>(
 	else if (ref) ref.current = value
 }
 
-export const useForkRef = <InstanceA, InstanceB>(
-	refA: Ref<InstanceA> | null | undefined,
-	refB: Ref<InstanceB> | null | undefined
-): Ref<InstanceA & InstanceB> | null => {
+export const useForkRef = <Instance>(
+	...refs: Array<Ref<Instance> | undefined>
+): RefCallback<Instance> | null  => {
 	/**
-	 * This will create a new function if the ref props change and are defined.
+	 * This will create a new function if the refs passed to this hook change and are all defined.
 	 * This means react will call the old forkRef with `null` and the new forkRef
 	 * with the ref. Cleanup naturally emerges from this behavior.
 	 */
 	return useMemo(() => {
-		if (refA == null && refB == null) {
+		if (refs.every((ref) => ref == null)) {
 			return null
 		}
-		return refValue => {
-			setRef(refA, refValue)
-			setRef(refB, refValue)
+
+		return (instance) => {
+			refs.forEach((ref) => {
+				setRef(ref, instance)
+			})
 		}
-	}, [refA, refB])
+	}, refs)
 }
